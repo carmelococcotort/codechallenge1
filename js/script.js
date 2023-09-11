764fa55810825443faa842f890d154f297bfdc8c
Martín Pereira <veridicotp22@gmail.com>
2023-09-11 15:12:40 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    let sendBtn = document.getElementById('enviar');
+
+    const URL = 'https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/grupo265';
+    sendBtn.addEventListener('click', function(){
+        let firstName = document.getElementById('nombre').value;
+        let lastName = document.getElementById('apellido').value;
+        let group = document.getElementById('grupo').value;
+        let room = document.getElementById('sala').value;
+
+        fetch(URL, {
+            headers: {"Content-Type": "application/json; charset=utf-8"},
+            method: 'POST',
+            body: JSON.stringify({
+                nombre: firstName,
+                apellido: lastName,
+                grupo: group,
+                sala: room
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+    });
+})
